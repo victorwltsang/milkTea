8b32fe5997a212a883782444106030613e3184cc
Coding <coding@JobTrainworks.org>
2016-10-06 15:11:41 -0700

    milk Tea

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('#greenTea').on("click", function() {
+      $('#tea').css({ fill: "#a9cc97" });
+  });
+  $('#redTea').on("click", function() {
+      $('#tea').css({ fill: "#cc4807" });
+  });
+  $('#blackTea').on("click", function() {
+      $('#tea').css({ fill: "#ac8f7d" });
+  });
+
+
+});
